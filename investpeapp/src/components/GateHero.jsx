44c7d3b05392dcb7f19a52c4d8767c23e8db9077
Pritam <borolepritam@gmail.com>
2023-08-26 13:00:37 +0530

    Gateway Hero Completed

--- investpeapp/src/components/GateHero.jsx
+++ investpeapp/src/components/GateHero.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import styles from '../styles/Gateway.module.css'
+import {FaChevronCircleDown} from 'react-icons/fa'
+import p from '../images/p.png'
 
 export default function GateHero() {
   return (
     <>
-    <div>
-
+    <div className={styles.hero}>
+      <div>
+        <p><span>INVESTMENT STACK</span> for India</p>
+        <p>Elevate Your Investment Strategy with Our <span>Comprehensive APIs</span></p>
+      </div>
+      <div>
+        <p>Launch financial services in a matter of days. Take your pick from our flexible APIs, SDKs, or ready-to-use screens.</p>
+        <p>See our products  <FaChevronCircleDown /></p>
+        <button>Contact Us </button>
+      </div>
     </div>
-    <div>
-        
+    <div className={styles.cards}>
+        <div>
+        <img src={p} />
+          <p>Stocks</p>
+        </div>
+        <div>
+          <img src={p} />
+          <p>Mutual Funds</p>
+        </div>
+        <div>
+        <img src={p} />
+          <p>Digital Gold</p>
+        </div>
     </div>
     </>
   )
--- investpeapp/src/pages/Gateway.jsx
+++ investpeapp/src/pages/Gateway.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Ticker from '../components/Ticker'
 import Footer from '../components/Footer'
 import GateNav from '../components/GateNav'
+import GateHero from '../components/GateHero'
 
 export default function Gateway() {
   return (
     <>
     <Ticker />
     <GateNav />
+    <GateHero />
     <Footer />
     </>
   )
